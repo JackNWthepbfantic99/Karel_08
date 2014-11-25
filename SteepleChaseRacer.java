d3d014cb2e65808a7d9a07d46de62195a89813e6
JackNWthepbfantic99 <staceynavin@Jacks-MacBook-Air-2.local>
2014-11-25 13:10:48 +0000

    then can hurdle

--- SteepleChaseRacer.java
+++ SteepleChaseRacer.java
@@ -1,8 +1,8 @@
 /**
  * A SteepleChaseRacer is a Racer that can jump over hurdles of any height.
  * 
- * @author <...>
- * @version <...>
+ * @author Jack Navin-Weinstein
+ * @version Tuesday 25 November
  *
  */
 public class SteepleChaseRacer extends Racer {
@@ -10,12 +10,27 @@ public class SteepleChaseRacer extends Racer {
 	//TODO Subclasses do not inherit any constructors from their superclass
 	//Consequently, you will need to provide a compatible constructor
 	
+   public SteepleChaseRacer(int y){
+      super(y);
+   }
 	/**
 	 * Re-implement the method to allow for jumping hurdles of any height
 	 */
 	@Override
-	public void jumpRight() {
-		
+	public void jumpRight(){
+		turnLeft();
+      int n = 0;
+   while(!rightIsClear()){
+      move();
+      n++;
+   }
+      turnRight();
+      move();
+      turnRight();
+   for(int k = 1; k <= n; k++){
+      move();
+   }
+   turnLeft();
 	}
 
 }
